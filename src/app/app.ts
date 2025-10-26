c708b91b8cf0e74b4a579d3be885f8f9e957d12b
ankita <ankita.s33@gmail.com>
2025-10-26 04:02:10 +0100

    styles: re org styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,16 @@
 import {Component, inject} from '@angular/core';
 import {Router, RouterLink, RouterOutlet} from '@angular/router';
 import {ThemeSelector} from '../theme-selector/theme-selector';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, ThemeSelector],
+  imports: [RouterOutlet, RouterLink, ThemeSelector, MatIcon],
   template: `
    <header class="main-header">
       @if (router.url !== '/') {
         <a routerLink="/" class="back-and-title">
-          <svg class="back-icon" viewBox="0 0 24 24"><path d="M20 11H7.8l5.6-5.6L12 4l-8 8 8 8 1.4-1.4L7.8 13H20v-2z"></path></svg>
+          <mat-icon>arrow_back</mat-icon>
           <span class="main-header-text">Demo site</span>
         </a>
       } @else {
